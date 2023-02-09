7f8385804ff61492dbdbac316e75fad285c1ec72
akimyou <akimyou@icloud.com>
2023-02-09 14:51:05 +0800

    update docs

--- site/components/DemoCode.tsx
+++ site/components/DemoCode.tsx
@@ -3,14 +3,40 @@ import React, { useState } from "react";
 export const DemoCode = (props: {
   children?: React.ReactNode;
   code?: React.ReactNode;
+  codeRaw?: string;
 }) => {
   const [showCode, setShowCode] = useState<boolean>(false);
+  const [showCopySuccess, setShowCopySuccess] = useState<boolean>(false);
   return (
     <div>
       {props.children}
       <p>
-        <a href="#" onClick={() => setShowCode(!showCode)}>
+        <a
+          href="#"
+          onClick={(event) => {
+            event.preventDefault();
+            setShowCode(!showCode);
+          }}
+        >
           {showCode ? "Hide Code" : "View Code"}
+        </a>
+        <a
+          href="#"
+          onClick={async (event) => {
+            event.preventDefault();
+            try {
+              await navigator.clipboard.writeText(props.codeRaw || "");
+              setShowCopySuccess(true);
+              setTimeout(() => {
+                setShowCopySuccess(false);
+              }, 2000);
+            } catch (error) {
+              console.error(error);
+            }
+          }}
+          style={{ marginLeft: 8 }}
+        >
+          {showCopySuccess ? "Copy Success" : "Copy Code"}
         </a>
       </p>
       {showCode && <div>{props.code}</div>}
